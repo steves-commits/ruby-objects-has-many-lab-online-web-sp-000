78133eaefd7b11dd68bb51c9784aed4bf03a766d
Steven Topazio <sat@northpointprinting.com>
2020-10-06 14:15:43 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song 
+  
+  attr_accessor :name, :artist 
+  
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def artist_name
+   if @artist == nil 
+     return nil 
+   else 
+     @artist.name 
+   end 
+ end
+end 
+
