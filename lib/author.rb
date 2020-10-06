c9c36e5d64332f1a5441cd587338d2deee7c844f
Steven Topazio <sat@northpointprinting.com>
2020-10-06 14:44:14 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -20,9 +20,7 @@ class Author
   
   def add_post_by_title(title)
     post = Post.new(title)
-    @posts << post 
-    post.author = self 
-    @@post_count += 1 
+    post.author = self
   end
 
   def self.post_count 
